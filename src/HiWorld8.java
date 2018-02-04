a568910b953ae7992d21003fbb403e1e7d6c5d00
Mike <Mike@10.0.0.245>
2018-02-03 23:20:32 -0500

    HW8 - Recommit 1

--- src/HiWorld8.java
+++ src/HiWorld8.java
@@ -3,7 +3,7 @@ public class HiWorld8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi World 8 Mike Eclipse - Recommit0!");
+		System.out.println("Hi World 8 Mike Eclipse - Recommit1!");
 		new HiWorld8Called().print8();
 	}
 }
