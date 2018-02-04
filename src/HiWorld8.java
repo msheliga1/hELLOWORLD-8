2a920b18195c40e972689c9e7eabce5c6df18eda
Mike <Mike@10.0.0.245>
2018-02-03 23:15:02 -0500

    Orig - "commit and push" 4

---
+++ src/HiWorld8.java
@@ -0,0 +1,10 @@
+
+public class HiWorld8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi World 8 Mike Eclipse - Recommit0!");
+		new HiWorld8Called().print8();
+	}
+}
+
---
+++ src/HiWorld8Called.java
@@ -0,0 +1,6 @@
+public class HiWorld8Called {
+	
+	void print8() { System.out.println("HeWorld8Called"); }
+
+}
+
